008b2afb28eddd33b20c91fbc72699df528cb5bc
WJvg82y6W6LAqXcJ <jVx7V9td>
2022-08-12 11:20:12 +0200

    dělení na předměty

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,8 @@ makedocs(
     sitename="Simulace a řízení mechatronických systémů",
     pages = [
         "index.md",
-        "Základní Pojmy Řízení.md",
+        "Řízené mechanické systémy" => [
+            "Základní Pojmy Řízení.md",
             "Stabilita lineárních systémů.md",
             "Říditelnost lineárního systému.md",
             "Pozorovatelnost lineárního systému.md",
@@ -15,6 +16,13 @@ makedocs(
             "Kritérium Optimality.md",
             "Pontrjaginův princip maxima.md",
             "Algebraické Riccatiho Rovnice.md",
+            ],
+        "Řízené aktivní struktury" => [
+            ],
+        "Simulace mechatronických systémů" => [
+            ],
+        "Syntéza a optimalizace mechanických systémů" => [
+            ]
         ]
 )
 
